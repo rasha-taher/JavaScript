09afd7e87d3de402436296003263430ee0c37268
rasha <rashataher1245@gmail.com>
2023-07-25 15:11:03 +0300

    Done with Task 2

---
+++ task2/script.js
@@ -0,0 +1,14 @@
+var name ="rasha";
+console.log(name);
+name='change';
+console.log(name);
+
+let name1="rasha"
+console.log(name1);
+name1="change";
+console.log(name1);
+
+const name2="rasha"
+console.log(name2);
+name2="change";
+console.log(name2);
