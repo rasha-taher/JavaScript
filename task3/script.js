9648fc00cee890db8ea4440a2d6917e4608ea7b2
rasha <rashataher1245@gmail.com>
2023-07-25 15:31:25 +0300

    Done with Task3

---
+++ task3/script.js
@@ -0,0 +1,15 @@
+const num1=5;
+var num2=6 ;
+let canPass=true;
+if(canPass==true){
+    if(num1>=num2){
+        console.log("hello");
+    }else {
+    console.log("hello");
+    }} 
+    else {
+    console.log("Try again");
+    }
+
+
+
