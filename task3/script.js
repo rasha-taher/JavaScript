bb256b3edfa680eaf29a88db4dfd57fce7a40b2b
rasha <rashataher1245@gmail.com>
2023-07-26 10:26:43 +0300

    Done with task 4

--- task3/script.js
+++ task3/script.js
@@ -1,11 +1,11 @@
 const num1=5;
-var num2=6 ;
+var num2=11 ;
 let canPass=true;
 if(canPass==true){
     if(num1>=num2){
         console.log("hello");
     }else {
-    console.log("hello");
+    console.log("Bye");
     }} 
     else {
     console.log("Try again");
---
+++ task4/script.js
@@ -0,0 +1,26 @@
+var arr1 = [1, 2, 3, 4, 5];
+console.log(arr1);
+
+var arr2=[];
+arr2[0]="name";
+arr2[1]="lastname";
+arr2[2]="phone number";
+arr2[3]="email";
+console.log(arr2[0]);
+console.log(arr2[1]);
+console.log(arr2[2]);
+console.log(arr2[3]);
+
+
+var arr3 =Array(1 , "Pizza", 2, "Burger", 4, 5 );
+console.log(arr3[0]);
+console.log(arr3[1]);
+console.log(arr3[2]);
+console.log(arr3[3]);
+console.log(arr3[4]);
+console.log(arr3[5]);
+
+
+
+
+console.log(arr1+arr2+arr3);
