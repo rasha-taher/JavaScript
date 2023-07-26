3f73cd32f8b329e50b5586baed5a5324006323b8
rasha <rashataher1245@gmail.com>
2023-07-26 12:22:32 +0300

    Done with task 5

---
+++ task5/script.js
@@ -0,0 +1,9 @@
+let arr =[5, 44, 2, 7, 8, 12, 24, 69, 71,4];
+
+for (let i=0; i<arr.length; i++){
+    if(arr[i]%2==0){
+        console.log(arr[i] + " This number is even");
+    }else {
+        console.log(arr[i]+ " This number is odd");
+    }
+}
